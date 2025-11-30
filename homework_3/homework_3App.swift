4f01392241a4500d27ee0b2ee7205b4191d05d26
Evgeniy-Bachilo <ebachilo@elinext.com>
2025-11-30 20:05:17 +0300

    Initial Commit

---
+++ homework_3/homework_3App.swift
@@ -0,0 +1,17 @@
+//
+//  homework_3App.swift
+//  homework_3
+//
+//  Created by Evgeniy Bachilo on 30.11.25.
+//
+
+import SwiftUI
+
+@main
+struct homework_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
